0bad15eeca0057932e8b7bb75b52349b1493ad3d
HYPERION-08 <hyperionnexus24@gmail.com>
2023-09-19 13:28:10 -0400

    added some code

--- Chapter_12_projects/Alien_invasion.py
+++ Chapter_12_projects/Alien_invasion.py
@@ -10,3 +10,14 @@ class AlienInvasion:
 
 		self.screen = pygame.display.set_mode((1200,800))
 		pygame.display.set_caption("Alien Invasion")
+
+	def run_game(self):
+		""" start the main loop for the game"""
+		while True:
+			# watch for keyboard and mouse events.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+			# make the most recently drawn screen visible
+			pygame.display.flip()
+
