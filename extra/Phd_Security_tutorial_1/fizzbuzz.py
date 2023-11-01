98fa1cca508ae0483549a2cc7ef28aa6c90b52eb
HYPERION-08 <hyperionnexus24@gmail.com>
2023-11-01 13:59:07 -0400

    simple challenge

---
+++ extra/Phd_Security_tutorial_1/fizzbuzz.py
@@ -0,0 +1,15 @@
+# FizzBuzz from (1 to 100)
+# if a number is divisible by 3, print fizz
+# if a numebr is divisible by 5, print buzz
+# if a number is divisible by both, print fizzbuzz
+
+
+for i in range(1,101):
+	if (i%3 == 0) and (i%5 == 0):
+		print(i,"fizzbuzz")
+	if i % 3 == 0:
+		print(i,'fizz')
+	elif i % 5 == 0:
+		print(i,'buzz')
+	else:
+		print(i)
