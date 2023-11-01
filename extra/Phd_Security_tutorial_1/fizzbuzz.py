890f56819917c3a356ab77e6f988dab5591a6a02
HYPERION-08 <hyperionnexus24@gmail.com>
2023-11-01 14:46:45 -0400

    venv environment added

--- extra/Phd_Security_tutorial_1/fizzbuzz.py
+++ extra/Phd_Security_tutorial_1/fizzbuzz.py
@@ -3,13 +3,23 @@
 # if a numebr is divisible by 5, print buzz
 # if a number is divisible by both, print fizzbuzz
 
+#
+# for i in range(1,101):
+# 	if (i%3 == 0) and (i%5 == 0):
+# 		print(i,"fizzbuzz")
+# 	if i % 3 == 0:
+# 		print(i,'fizz')
+# 	elif i % 5 == 0:
+# 		print(i,'buzz')
+# 	else:
+# 		print(i)
 
 for i in range(1,101):
 	if (i%3 == 0) and (i%5 == 0):
 		print(i,"fizzbuzz")
-	if i % 3 == 0:
-		print(i,'fizz')
-	elif i % 5 == 0:
-		print(i,'buzz')
+	elif i%3 == 0:
+		print(i,"fizz")
+	elif i%5 == 0:
+		print(i,"buzz")
 	else:
 		print(i)
